278efddd6c7d6f0be784bf570cc45a914d0245b0
Diego Vila <diego@velascommerce.com>
2023-11-17 09:16:29 -0400

    got service workers to cache

--- packages/app-spa/public/sw.js
+++ packages/app-spa/public/sw.js
@@ -1,5 +1,12 @@
 self.addEventListener("install", function (event) {
   console.log("SW Installed: ", event);
+
+  // wait untill cache completes
+  event.waitUntill(
+    cashes.open("static").then((cache) => {
+      cache.add("/src/main.tsx");
+    }),
+  );
 });
 
 self.addEventListener("activate", function (event) {
@@ -9,5 +16,14 @@ self.addEventListener("activate", function (event) {
 
 self.addEventListener("fetch", function (event) {
   console.log("SW fetching: ", event);
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      } else {
+        return fetch(event.request);
+      }
+    }),
+  );
+  // event.respondWith(fetch(event.request));
 });
